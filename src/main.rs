9004b9e391bd6bd419f75f90fd4a8a88030ec8d7
Isac Sund <isac.sund@outlook.com>
2020-06-07 13:36:51 +0300

    Setup database connection pool with Diesel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::PgConnection;
+
+use jobq::settings::Settings;
+
+#[tokio::main]
+async fn main() {
+    let settings = Settings::get();
+
+    // Set up the r2d2 connection pool
+    let manager = ConnectionManager::<PgConnection>::new(&settings.get_database_url());
+    let pool = Pool::builder()
+        .max_size(settings.database.pool_size)
+        .build(manager)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", settings.get_database_url()));
+
+    // Get the connection
+    let conn = pool.get().unwrap();
 }
---
+++ src/settings.rs
@@ -0,0 +1,47 @@
+use config::{Config, ConfigError, Environment, File};
+use serde::Deserialize;
+
+static CONFIG_FILE_DEFAULTS: &str = "config/settings.toml";
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Settings {
+    pub database: Database,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Database {
+    pub user: String,
+    pub password: String,
+    pub host: String,
+    pub port: i32,
+    pub database: String,
+    pub pool_size: u32,
+}
+
+impl Settings {
+    /// Reads settings from the config file.
+    fn init() -> Result<Self, ConfigError> {
+        let mut config = Config::new();
+
+        config.merge(File::with_name(CONFIG_FILE_DEFAULTS))?;
+
+        config.try_into()
+    }
+
+    /// Returns the config as a struct.
+    pub fn get() -> Self {
+        Settings::init().unwrap()
+    }
+
+    /// Returns the postgres connection url generated from the config.
+    pub fn get_database_url(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.database.user,
+            self.database.password,
+            self.database.host,
+            self.database.port,
+            self.database.database
+        )
+    }
+}
